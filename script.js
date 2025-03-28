e27172c62e2f84aca87ecb2381850cec08cef740
Abdeul Qadir <qadir20005@gmail.com>
2025-03-28 20:11:00 +0500

    some changing

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const zakatAmount = document.querySelector("#zakat_value")
 const calculationButton = document.querySelector("#calculation")
 const zakatValue = document.querySelector("#zakat_value") 
 
-const zakatFormula = ()=>{
-
+const zakatFormula = (e)=>{
+    e.preventdefault()
     
     // formula on zakat amount
     const zakatPercentage = 0.025 // 2.5 percentage 
     const freshZakatAmountValue = amount.value * zakatAmount
     
-    zakatAmount.innerHTML= freshZakatAmountValue
+    amount.innerHTML= freshZakatAmountValue
     
 }
+zakatFormula()
