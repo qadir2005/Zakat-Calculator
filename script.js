109a0449a1c42f69953904b9f787f4353ffa0c31
Abdeul Qadir <qadir20005@gmail.com>
2025-03-29 16:46:39 +0500

    some changing

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const amount = document.querySelector("#totalAmount")
-const zakatAmount = document.querySelector("#zakat_value")
-const calculationButton = document.querySelector("#calculation")
-const zakatValue = document.querySelector("#zakat_value") 
+const zakatInput = document.querySelector("#totalAmount") // Zakat input
+const calculation = document.querySelector("#calculation") // Zakat Ammount button
+const zakatResult = document.querySelector("#zakat_value") // zakat value yaha pr zakat ka result show hoga
 
-const zakatFormula = (e)=>{
-    e.preventdefault()
-    
-    // formula on zakat amount
-    const zakatPercentage = 0.025 // 2.5 percentage 
-    const freshZakatAmountValue = amount.value * zakatAmount
-    
-    amount.innerHTML= freshZakatAmountValue
-    
-}
-zakatFormula()
+
+const zakatFormula  = (e)=>{
+    e.preventDefault(
+
+    )
+
+    const  zakatPercentage = 0.025
+    const totalAmount = Number( zakatInput.value)
+
+    if(isNaN(totalAmount) || totalAmount <= 0){
+
+        zakatResult.innerHTML = "Please input the value number aby oye smaj nhi aya tgheek se kaam kr "
+        return
+    }
+
+    const zakatValue =   totalAmount*zakatPercentage.toFixed(2)
+
+    zakatResult.innerHTML = zakatValue  
+console.log("khuch tuo howa");
+
+
+} 
+
+calculation.addEventListener("click", zakatFormula)
